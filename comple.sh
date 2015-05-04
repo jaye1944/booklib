d387b32ba634950c6b2f73b37fd372526d3d9eac
jaye1944 <jaye1944@gmail.com>
2015-05-04 09:50:36 +0530

    add parcellite install

--- comple.sh
+++ comple.sh
@@ -1,5 +1,6 @@
 #sudo apt-get install libsqlite3-dev
 #sudo sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+#sudo apt-get install parcellite
 echo "compiling... "
 gcc main.c -lsqlite3 -o pp
 
