d0ab0446d9dee1404bfa99cb1661cd92e616f9e9
jaye1944 <jaye1944@gmail.com>
2015-05-10 14:44:35 +0530

    restructure project v0.1

--- comple.sh
+++ comple.sh
@@ -3,18 +3,23 @@
 #sudo apt-get install parcellite
 #http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/intro.html
 #http://www.cyberciti.biz/faq/bash-shell-change-the-color-of-my-shell-prompt-under-linux-or-unix/
+#http://www.pauloamgomes.net/blog/dropbox-shell-uploader
+#http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
 if [ $# -eq 0 ];
 then
+	tput setaf 3
 	echo "Usage is ./compile.sh [option]"
 	echo "Compile with sqlite './compile.sh s'"
 	echo "Compile with ncurses './compile.sh n'"
-	exit
+	tput sgr0
+	exit 0
 fi
 
 if [ $1 = "s" ];
 then
 echo "#### compiling with sqlite#### "
 gcc -Wall main.c -lsqlite3 -o pp
+#gcc -Wall create.c -lsqlite3 -o pp
 fi
 
 if [ $1 = "n" ];
