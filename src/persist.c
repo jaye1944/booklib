fbc36cc5bf95946a9a54a55eb37d1c14dfaa4f80
jaye1944 <jaye1944@gmail.com>
2015-05-24 13:58:07 +0530

    remove callback from sqlite_exe, [not need to create]

--- src/persist.c
+++ src/persist.c
@@ -10,34 +10,34 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 
 int createBookTable(char *zErrMsg) {
 	int bc, boc, st, uc, pc;
-	bc = sqlite3_exec(db, BOOK_Qu, callback, 0, &zErrMsg);
+	bc = sqlite3_exec(db, BOOK_Qu, NULL, 0, &zErrMsg);
 	if (bc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 		return 0;
 	}
-	boc = sqlite3_exec(db, BORROW_QU, callback, 0, &zErrMsg);
+	boc = sqlite3_exec(db, BORROW_QU, NULL, 0, &zErrMsg);
 	if (boc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 		return 0;
 	}
 
-	st = sqlite3_exec(db, STATUS_QU, callback, 0, &zErrMsg);
+	st = sqlite3_exec(db, STATUS_QU, NULL, 0, &zErrMsg);
 	if (st != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 		return 0;
 	}
 
-	uc = sqlite3_exec(db, USERS_QU, callback, 0, &zErrMsg);
+	uc = sqlite3_exec(db, USERS_QU, NULL, 0, &zErrMsg);
 	if (uc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 		return 0;
 	}
 
-	pc = sqlite3_exec(db, PLACE_QU, callback, 0, &zErrMsg);
+	pc = sqlite3_exec(db, PLACE_QU, NULL, 0, &zErrMsg);
 	if (pc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
